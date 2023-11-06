e52e47a120a815cc55caf1a6abb15092d4bbc1b6
Udit Verma <uditverma150@gmail.com>
2023-11-07 00:19:46 +0530

    Add project files

---
+++ schema.js
@@ -0,0 +1,23 @@
+// For validation of server side schema - it is different 
+//                                        from our mongoose schema
+// For this we use "joi" i.e. an npm package which helps in schema validation\
+
+const Joi = require("joi");
+
+module.exports.listingSchema = Joi.object({
+    listing: Joi.object({
+        title: Joi.string().required(),
+        description: Joi.string().required(),
+        location: Joi.string().required(),
+        country: Joi.string().required(),
+        price: Joi.number().required().min(0),
+        image: Joi.string().allow("", null)
+    }).required()
+});
+
+module.exports.reviewSchema = Joi.object({
+    review: Joi.object({
+        rating: Joi.number().required().min(1).max(5),
+        comment: Joi.string().required()
+    }).required()
+});
